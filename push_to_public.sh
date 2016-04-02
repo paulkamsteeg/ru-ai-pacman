de0699176dda795d2cc954e34569f63fa0eaca8c
Marein Könings <mail@marein.org>
2016-04-02 12:54:58 +0200

    add push script

---
+++ push_to_public.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This script is used to push from the internal repository to the public repository.
+
+REPO="https://github.com/MareinK/ru-ai-pacman.git"
+
+echo "Attempting to push to public repository..."
+git push $REPO
