e3d7f314da8ad83b03a1ec2d84dec44c1380050d
Chris Kamphuis <mail@chriskamphuis.com>
2016-12-14 22:49:31 +0100

    PEP 8 week 1

--- 1-python/buyLotsOfFruit.py
+++ 1-python/buyLotsOfFruit.py
@@ -1,15 +1,15 @@
+fruitPrices = {'apples': 2.00, 'oranges': 1.50, 'pears': 1.75,
+               'limes': 0.75, 'strawberries': 1.00}
 
-fruitPrices = {'apples':2.00, 'oranges': 1.50, 'pears': 1.75,
-              'limes':0.75, 'strawberries':1.00}
 
 def buyLotsOfFruit(orderList):
     """
         orderList: List of (fruit, numPounds) tuples
-            
+
     Returns cost of order
-    """          
-	
+    """
+
     " YOUR CODE HERE "
 
-orderList = [ ('apples', 2), ('pears', 3), ('limes', 4) ]
+orderList = [('apples', 2), ('pears', 3), ('limes', 4)]
 print('Cost of', orderList, 'is', buyLotsOfFruit(orderList))
