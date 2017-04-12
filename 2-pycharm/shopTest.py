17364025b6937746e6f28c0566ac6f8d524c4084
GijsDekkers <gijs-dekkers@live.nl>
2017-04-12 11:11:09 +0200

    fouten shopTest

--- 2-pycharm/shopTest.py
+++ 2-pycharm/shopTest.py
@@ -5,11 +5,11 @@ aldiPrices = {'apples': 1.00, 'oranges': 1.50, 'pears': 1.75}
 aldiShop = shop.FruitShop(aldiName, aldiPrices)
 aldiApples = aldiShop.getCostPerPound('apples')
 print(aldiApples)
-print('Apples cost €%.2f at %s.' % (aldiApples, aldiName))
+print('Apples cost %.2f at %s.' % (aldiApples, aldiName))
 
 albertName = 'Albert Heijn'
 albertPrices = {'kiwis':6.00, 'apples': 4.50, 'peaches': 8.75}
 albertShop = shop.FruitShop(albertName, albertPrices)
 albertApples = albertShop.getCostPerPound('apples')
 print(albertApples)
-print('Apples cost €%.2f at %s.' % (albertApples, albertName))
+print('Apples cost %.2f at %s.' % (albertApples, albertName))
